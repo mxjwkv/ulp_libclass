df4fb2fca64b5f5cd8271c2eafba6c262af11397
mxjwkv <mxjwkv@gmail.com>
2015-09-01 15:02:59 -0700

    Add ulp_libclass

---
+++ __build_num_release.h
@@ -0,0 +1,11 @@
+// __build_num_release.h
+
+// Created: 2015-08-28 16:29:56 by __incrbuildnum (Ver 2)
+
+#ifndef _BUILD_NUM_H_
+#define _BUILD_NUM_H_
+
+#define BUILD_NUM 2
+#define BUILD_NUM_TXT "2"
+
+#endif // _BUILD_NUM_H_
---
+++ lin_build.sh
@@ -0,0 +1,3 @@
+#lin_build.sh 02-AUG-2012 sds
+g++ -c *.cpp
+ar -rvs ../lib/ulp_libclass.a *.o
