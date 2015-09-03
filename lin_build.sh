a872286c749b06f95721f06bb530482840290e33
mxjwkv <mxjwkv@gmail.com>
2015-09-02 19:05:52 -0700

    Update libclass with Release and Debug builds

--- lin_build.sh
+++ lin_build.sh
@@ -1,3 +1,9 @@
-#lin_build.sh 02-AUG-2012 sds
-g++ -c *.cpp
-ar -rvs ../lib/ulp_libclass.a *.o
+# lin_build.sh
+#     Created: 02-AUG-2012
+# Last Update: 02-SEP-2015
+echo --- Release ---------------------------------------------
+g++ -Wall -O2 -c *.cpp
+ar -rvs ../lib/libulp_libclass.a *.o
+echo --- Debug -----------------------------------------------
+g++ -Wall -g -c *.cpp
+ar -rvs ../lib/libulp_libclassD.a *.o
