312715cb2e9128fc586142687db865221128ae18
mxjwkv <mxjwkv@gmail.com>
2015-09-03 18:46:27 -0700

    Update prototype include file

--- libclass.h
+++ libclass.h
@@ -1,8 +1,10 @@
 // libclass.h
 //     Created: 26-AUG-2015
-// Last Update: 26-AUG-2015
+// Last Update: 03-SEP-2015
 
 //-----------------------------------------------------------------------------
 // Library Class prototypes
+char *getLibClassBuildInfo(char *szBuildInfo, int nBuildInfoLen);
 
 #include "x.h"
+
